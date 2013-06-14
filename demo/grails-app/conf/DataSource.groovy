ec2b273f2a32ae4df5d2023611ec124bd9087366
Roberto Perez <pr@manoderecha.mx>
2013-06-14 18:20:56 -0500

    DataSource: configurado

--- demo/grails-app/conf/DataSource.groovy
+++ demo/grails-app/conf/DataSource.groovy
@@ -13,8 +13,28 @@ hibernate {
 environments {
     development {
         dataSource {
+            pooled = true
+            driverClassName = "com.mysql.jdbc.Driver"
+            username = "demo"
+            password = 'demo'
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/demo?autoReconnect=true"
+            properties {
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 1
+                initialSize = 1
+                minEvictableIdleTimeMillis = 60000
+                timeBetweenEvictionRunsMillis = 60000
+                numTestsPerEvictionRun = 3
+                maxWait = 10000
+
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+
+                validationQuery = "SELECT 1"
+            }
         }
     }
     test {
@@ -25,18 +45,27 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
             pooled = true
+            driverClassName = "com.mysql.jdbc.Driver"
+            username = "demo"
+            password = 'demo'
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/demo?autoReconnect=true"
             properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 1
+                initialSize = 1
+                minEvictableIdleTimeMillis = 60000
+                timeBetweenEvictionRunsMillis = 60000
+                numTestsPerEvictionRun = 3
+                maxWait = 10000
+
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+
+                validationQuery = "SELECT 1"
             }
         }
     }
